a09eb5c7e447a09ab3734b77fdc7cd8622a5e39b
Erin Parker <erinchanparker@gmail.com>
2012-11-25 19:05:34 -0800

    Revert "Working on adding exercises"
    This reverts commit b318fc7dc0d4c5f6b3dab741bbdc6889e7365124.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,10 +16,7 @@ class UsersController < ApplicationController
   # GET /users/1.json
   def show
     @user = User.find(params[:id])
-    
-    @exercises = @user.exercises 
-    @exercise = Exercise.new
-
+    @exercises = @user.exercises  
     @microposts = @user.microposts
     @micropost = @user.microposts.build
 
